ee24744897e237b23ed23d8525fc5a17c67936f0
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-03-12 16:30:45 -0500

    fixing login

--- resources/js/composables/permissions.js
+++ resources/js/composables/permissions.js
@@ -3,7 +3,7 @@ import { usePage } from "@inertiajs/vue3";
 
 export function usePermission(){
     const hasRole = (name) => usePage().props.auth.user.roles.includes(name);
-    const hasPermissions = (name) => usePage().props.auth.user.includes(name);
+    const hasPermissions = (name) => usePage().props.auth.user.permissions.includes(name);
 
     return { hasRole, hasPermissions};
 }
